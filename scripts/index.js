bf23e0d61718d13157a90295794ad19c2dc9119b
Julie Chen <juliechenmedicine@gmail.com>
2024-01-29 10:42:15 +0900

    faeture javascript cardElement

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,7 @@ const initialCards = [
   },
 ];
 console.log(initialCards);
-
+/*elements*/
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditModalCloseBtn = document.querySelector(
@@ -38,20 +38,46 @@ const profileDescriptionInput = document.querySelector(
 const profileName = document.querySelector("#js-profile-name");
 const profileDescription = document.querySelector("#js-profile-description");
 const profileForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemp =
+  document.querySelector("#card-template").content.firstElementChild;
 
+/*function & handlers*/
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
 }
 
+function handleProfileSubmit(e) {
+  e.preventDefault();
+  profileName.innerText = profileTitleInput.value;
+  profileDescription.innerText = profileDescriptionInput.value;
+  closePopup();
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemp.cloneNode(true);
+  const cardTitle = data.name;
+  const cardLink = data.link;
+  const cardTitleEl = cardElement.querySelector(".card__description");
+  cardTitleEl.innerText = cardTitle;
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = cardLink;
+  cardImageEl.alt = cardTitle;
+  return cardElement;
+}
+
+/*event listeners*/
 profileEditBtn.addEventListener("click", function () {
   profileTitleInput.value = profileName.innerText;
   profileDescriptionInput.value = profileDescription.innerText;
   profileEditModal.classList.add("modal_opened");
 });
+
 profileEditModalCloseBtn.addEventListener("click", closePopup);
-profileForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  profileName.innerText = profileTitleInput.value;
-  profileDescription.innerText = profileDescriptionInput.value;
-  closePopup();
+
+profileForm.addEventListener("submit", handleProfileSubmit);
+
+initialCards.forEach((data) => {
+  cardElement = getCardElement(data);
+  cardListEl.append(cardElement);
 });
